8004fe652e4955be63647c0690653658a00a1cee
kotabrog <suzuki.kota0331@gmail.com>
2023-05-01 11:52:54 +0900

    add add and mul backward

--- rust_dezero/src/function/operator/add.rs
+++ rust_dezero/src/function/operator/add.rs
@@ -39,7 +39,21 @@ impl FunctionContents for Add {
     }
 
     fn get_backward(&self) -> fn(usize, &mut FunctionTable, &mut VariableTable) -> Vec<usize> {
-        todo!()
+        |function_id, function_table, variable_table| {
+            let function = function_table.get_function(function_id).expect("Invalid function id");
+            let inputs = function.get_inputs().expect("Invalid inputs");
+            let outputs = function.get_outputs().expect("Invalid outputs");
+            Add::input_check(inputs);
+            Add::output_check(outputs);
+            let input_ids = inputs.clone();
+            let output_id = outputs[0];
+            let output_grad_id = variable_table.get_variable_grad_id(output_id).expect("Output grad id not found");
+
+            variable_table.update_grad(input_ids[0], output_grad_id, function_table);
+            variable_table.update_grad(input_ids[1], output_grad_id, function_table);
+
+            input_ids
+        }
     }
 }
 
@@ -60,10 +74,33 @@ mod tests {
             Tensor::new_from_num_vec(data0.clone(), vec![3]), "x");
         let id1 = variable_table.generate_variable_from_f64_tensor(
             Tensor::new_from_num_vec(data1.clone(), vec![3]), "y");
-        
+
         let output_ids = function_table.forward(square_id, vec![id0, id1], &mut variable_table, false);
 
         let output = variable_table.get_variable_contents_f64(output_ids[0]).unwrap();
-        assert_eq!(output.data(), Tensor::new_from_num_vec(vec![5.0, 7.0, 9.0], vec![3]).data());
+        assert_eq!(output, &Tensor::new_from_num_vec(vec![5.0, 7.0, 9.0], vec![3]));
+    }
+
+    #[test]
+    fn backward_normal() {
+        let mut variable_table = VariableTable::new();
+        let mut function_table = FunctionTable::new();
+
+        let data0 = vec![1.0, 2.0, 3.0];
+        let data1 = vec![4.0, 5.0, 6.0];
+        let add_id = function_table.generate_function_from_function_contents(Box::new(Add::new()));
+        let id0 = variable_table.generate_variable_from_f64_tensor(
+            Tensor::new_from_num_vec(data0.clone(), vec![3]), "x");
+        let id1 = variable_table.generate_variable_from_f64_tensor(
+            Tensor::new_from_num_vec(data1.clone(), vec![3]), "y");
+
+        let output_ids = function_table.forward(add_id, vec![id0, id1], &mut variable_table, false);
+
+        variable_table.backward(output_ids, &mut function_table, false);
+
+        let grad0 = variable_table.get_variable_grad_contents_f64(id0).unwrap();
+        let grad1 = variable_table.get_variable_grad_contents_f64(id1).unwrap();
+        assert_eq!(grad0, &Tensor::new_from_num_vec(vec![1.0, 1.0, 1.0], vec![3]));
+        assert_eq!(grad1, &Tensor::new_from_num_vec(vec![1.0, 1.0, 1.0], vec![3]));
     }
 }
--- rust_dezero/src/function/operator/mul.rs
+++ rust_dezero/src/function/operator/mul.rs
@@ -39,7 +39,26 @@ impl FunctionContents for Mul {
     }
 
     fn get_backward(&self) -> fn(usize, &mut FunctionTable, &mut VariableTable) -> Vec<usize> {
-        todo!()
+        |function_id, function_table, variable_table| {
+            let function = function_table.get_function(function_id).expect("Invalid function id");
+            let inputs = function.get_inputs().expect("Invalid inputs");
+            let outputs = function.get_outputs().expect("Invalid outputs");
+            Mul::input_check(inputs);
+            Mul::output_check(outputs);
+            let input_ids = inputs.clone();
+            let output_id = outputs[0];
+            let output_grad_id = variable_table.get_variable_grad_id(output_id).expect("Output grad id not found");
+
+            let mul_id0 = function_table.generate_function_from_function_contents(Box::new(Mul::new()));
+            let grad_id0 = function_table.forward(mul_id0, vec![output_grad_id, input_ids[1]], variable_table, false)[0];
+            let mul_id1 = function_table.generate_function_from_function_contents(Box::new(Mul::new()));
+            let grad_id1 = function_table.forward(mul_id1, vec![output_grad_id, input_ids[0]], variable_table, false)[0];
+
+            variable_table.update_grad(input_ids[0], grad_id0, function_table);
+            variable_table.update_grad(input_ids[1], grad_id1, function_table);
+
+            input_ids
+        }
     }
 }
 
@@ -65,5 +84,28 @@ mod tests {
 
         let output = variable_table.get_variable_contents_f64(output_ids[0]).unwrap();
         assert_eq!(output.data(), Tensor::new_from_num_vec(vec![4.0, 10.0, 18.0], vec![3]).data());
+    }
+
+    #[test]
+    fn backward_normal() {
+        let mut variable_table = VariableTable::new();
+        let mut function_table = FunctionTable::new();
+
+        let data0 = vec![1.0, 2.0, 3.0];
+        let data1 = vec![4.0, 5.0, 6.0];
+        let mul_id = function_table.generate_function_from_function_contents(Box::new(Mul::new()));
+        let id0 = variable_table.generate_variable_from_f64_tensor(
+            Tensor::new_from_num_vec(data0.clone(), vec![3]), "x");
+        let id1 = variable_table.generate_variable_from_f64_tensor(
+            Tensor::new_from_num_vec(data1.clone(), vec![3]), "y");
+
+        let output_ids = function_table.forward(mul_id, vec![id0, id1], &mut variable_table, false);
+
+        variable_table.backward(output_ids, &mut function_table, false);
+
+        let grad0 = variable_table.get_variable_grad_contents_f64(id0).unwrap();
+        let grad1 = variable_table.get_variable_grad_contents_f64(id1).unwrap();
+        assert_eq!(grad0, &Tensor::new_from_num_vec(vec![4.0, 5.0, 6.0], vec![3]));
+        assert_eq!(grad1, &Tensor::new_from_num_vec(vec![1.0, 2.0, 3.0], vec![3]));
     }
 }
